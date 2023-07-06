b10822036669306d641fbb118bfff716a3de73c5
karansuryawanshi <98444090+karansuryawanshi@users.noreply.github.com>
2023-07-06 14:43:08 +0530

    Update client.js

--- frontend/src/client.js
+++ frontend/src/client.js
@@ -2,11 +2,11 @@ import sanityClient from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
 export const client = new sanityClient({
-  projectId: portfolio-inky-two-96.vercel.app,
+  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
   dataset: "production",
   apiVersion: "2022-02-01",
   useCdn: true,
-  token: process.env.REACT_APP_SANITY_TOKEN,
+  token: portfolio-inky-two-96.vercel.app,
 });
 
 const builder = imageUrlBuilder(client);
