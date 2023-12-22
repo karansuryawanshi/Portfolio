7739f19fe87147f454b791263148916d24d8c32b
karansuryawanshi <karansuryawanshi717@gmail.com>
2023-12-23 00:35:36 +0530

    My first commit

--- frontend/src/containers/Work/Work.jsx
+++ frontend/src/containers/Work/Work.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { AiFillEye, AiFillGithub } from 'react-icons/ai';
 import { motion } from 'framer-motion';
-import { AppWrap,MotionWrap } from '../../wrapper';
+import { AppWrap, MotionWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
 import './Work.scss';
 
@@ -20,12 +20,27 @@ const Work = () => {
     });
   }, []);
 
+  // const handleWorkFilter = (item) => {
+  //   setActiveFilter(item);
+  //   setAnimateCard([{ y: 100, opacity: 0 }]);
+
+  //   setTimeout(() => {
+  //     setAnimateCard([{ y: 0, opacity: 1 }]);
+  //     if (item === 'All') {
+  //       setFilterWork(works);
+  //     } 
+  //     else {
+  //       setFilterWork(works.filter((work) => work.tags.includes(item)));
+  //     }
+  //   }, 500);
+  // };
+
   const handleWorkFilter = (item) => {
     setActiveFilter(item);
-    setAnimateCard([{ y: 100, opacity: 0 }]);
+    setAnimateCard({ y: 100, opacity: 0 });
 
     setTimeout(() => {
-      setAnimateCard([{ y: 0, opacity: 1 }]);
+      setAnimateCard({ y: 0, opacity: 1 });
       if (item === 'All') {
         setFilterWork(works);
       } else {
@@ -36,11 +51,11 @@ const Work = () => {
 
   return (
     <>
-      <h2 className="head-text">My Creative <span>Portfolio</span> Section</h2>
+      <h2 className="head-text">My Creative <span>Project</span> Section</h2>
       <div className="app__work-filter">
-        {['UI/UX', 'Web App', 'Webpage', 'React JS', 'All'].map((item, index) => (
+        {['UI/UX', 'Next JS', 'AI', 'React JS', 'All'].map((item, index) => (
           <div
-          key={index}
+            key={index}
             onClick={() => handleWorkFilter(item)}
             className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}
           >
@@ -59,7 +74,9 @@ const Work = () => {
             <div
               className="app__work-img app__flex"
             >
-              <img src={urlFor(work.imgUrl)} alt={work.name} />
+              {work.imgUrl && (
+                <img src={urlFor(work.imgUrl)} alt={work.name} />
+              )}
               <motion.div
                 whileHover={{ opacity: [0, 1] }}
                 transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
@@ -91,7 +108,9 @@ const Work = () => {
               <h4 className="bold-text">{work.title}</h4>
               <p className="p-text" style={{ marginTop: 10 }}>{work.description}</p>
               <div className="app__work-tag app__flex">
-                <p className="p-text">{work.tags[0]}</p>
+                {work.tags && work.tags[0] && (
+                  <p className="p-text">{work.tags[0]}</p>
+                )}
               </div>
             </div>
           </div>
