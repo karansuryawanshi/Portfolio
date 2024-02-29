6b209155e26776ce3abfcb7092c2ce0fdac39cbf
karansuryawanshi <karansuryawanshi717@gmail.com>
2024-02-29 18:14:42 +0530

    My first commit

--- frontend/src/containers/Work/Work.jsx
+++ frontend/src/containers/Work/Work.jsx
@@ -82,26 +82,30 @@ const Work = () => {
                 transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
                 className="app__work-hover app__flex"
               >
+                {work.projectLink &&
                 <a href={work.projectLink} target="_blank" rel="noreferrer">
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
                     whileHover={{ scale: [1, 0.90] }}
                     transition={{ duration: 0.25 }}
                     className="app__flex"
-                  >
+                    >
                     <AiFillEye />
                   </motion.div>
                 </a>
+                  }
+                {work.codeLink &&
                 <a href={work.codeLink} target="_blank" rel="noreferrer">
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
                     whileHover={{ scale: [1, 0.90] }}
                     transition={{ duration: 0.25 }}
                     className="app__flex"
-                  >
+                    >
                     <AiFillGithub />
                   </motion.div>
                 </a>
+                  }
               </motion.div>
             </div>
             <div className="app__work-content app__flex">
